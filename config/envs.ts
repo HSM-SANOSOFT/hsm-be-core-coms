adc3982530d3f394d93b45efea842e061949d0e5
Raul Santamaria <raulsantamariao@hotmail.com>
2025-01-29 18:22:53 -0500

    Refactor database connection handling and update ESLint configuration

--- config/envs.ts
+++ config/envs.ts
@@ -12,13 +12,14 @@ dotenv.config({
 });
 
 interface EnvVars {
+  COMS_MICROSERVICE_NAME: string;
   COMS_MICROSERVICE_HOST: string;
   COMS_MICROSERVICE_PORT: number;
 
   DB_USER: string;
   DB_PASSWORD: string;
   DB_CONNECTION_STRING: string;
-  ORACLE_CLIENT_PATH: string;
+  LD_LIBRARY_PATH: string;
 
   MAIL_HOST: string;
   MAIL_PORT: string;
@@ -29,13 +30,14 @@ interface EnvVars {
 
 const envsSchema = joi
   .object({
+    COMS_MICROSERVICE_NAME: joi.string().required(),
     COMS_MICROSERVICE_HOST: joi.string().default('localhost'),
     COMS_MICROSERVICE_PORT: joi.number().required(),
 
     DB_USER: joi.string().required(),
     DB_PASSWORD: joi.string().required(),
     DB_CONNECTION_STRING: joi.string().required(),
-    ORACLE_CLIENT_PATH: joi.string().default('C:/ORACLE/instantclient_12_1'),
+    LD_LIBRARY_PATH: joi.string().default('C:/ORACLE/instantclient_12_1'),
 
     MAIL_HOST: joi.string().required(),
     MAIL_PORT: joi.string().required(),
@@ -55,13 +57,14 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
+  COMS_MICROSERVICE_NAME: envVars.COMS_MICROSERVICE_NAME,
   COMS_MICROSERVICE_HOST: envVars.COMS_MICROSERVICE_HOST,
   COMS_MICROSERVICE_PORT: envVars.COMS_MICROSERVICE_PORT,
 
   DB_USER: envVars.DB_USER,
   DB_PASSWORD: envVars.DB_PASSWORD,
   DB_CONNECTION_STRING: envVars.DB_CONNECTION_STRING,
-  ORACLE_CLIENT_PATH: envVars.ORACLE_CLIENT_PATH,
+  LD_LIBRARY_PATH: envVars.LD_LIBRARY_PATH,
 
   MAIL_HOST: envVars.MAIL_HOST,
   MAIL_PORT: envVars.MAIL_PORT,
