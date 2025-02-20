c68450e6958b535078b65a1bc1d1926a81011f21
Hamilton <hamiltonxb3.3@gmail.com>
2025-02-20 16:39:47 -0500

    feat: enhance email service with template retrieval and creation functionality

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -11,5 +11,28 @@ export class EmailsService {
     private readonly databaseService: DatabaseService,
     private readonly mailerService: MailerService,
   ) {}
-  async sendEmail(body: SendEmailDto) {}
+  async sendEmail(body: SendEmailDto) {
+    const { to, template } = body;
+    try {
+      const template_elegida = await this.databaseService.query({
+        label: 'GET EMAIL TEMPLATE',
+        sql: `SELECT * FROM PDP_EMAIL_PLANTILLA WHERE ID = :id`,
+        params: [template],
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+  async create_email_template(body: any) {
+    const { nombre, version, app } = body;
+    try {
+      await this.databaseService.query({
+        label: 'CREATE EMAIL TEMPLATE',
+        sql: `INSERT INTO PDP_EMAIL_PLANTILLA (NOMBRE, VERSION, APP) VALUES (:nombre, :version, :app)`,
+        params: [nombre, version, app],
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
 }
